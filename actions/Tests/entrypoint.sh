ec6ecd7870293bd111d4c64b3cd48ad4243cb60c
ismaelGTI <ismaelmarin11@uma.es>
2025-04-03 12:44:29 +0200

    OK

---
+++ actions/Tests/entrypoint.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+echo "Recibira dos parámetros y tendrá un echo ""Ejecutando tests param 1 y param 2"
---
+++ app/app1.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+# Script de aplicación sencilla que imprime "Hola Mundo"
+echo "Hola Mundo, soy Ingeniero de Accenture. "
